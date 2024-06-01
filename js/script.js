f7d4abec1ec5525f52b28e4de749ab1e48a864bf
martinaliporata <martinaliporata@gmail.com>
2024-06-01 17:36:08 +0200

    added container with sidebar and chats with flexbox

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const {createApp} = Vue
+
+createApp ({
+    data(){
+        return {
+            // contatti: [
+            //     {
+            //         name: 'Michele',
+            //         image: 
+            //     },
+            //     {
+            //         name: 'Fabio',
+            //         image:
+            //     },
+            //     {
+            //         name: 'Samuele',
+            //         image:
+            //     },
+            //     {
+            //         name: 'Alessandro B.',
+            //         image:
+            //     },
+            //     {
+            //         name: 'Alessandro L.',
+            //         imageUrl: './imgs/avatar_5.jpg',
+            //     },
+            //     {
+            //         name: 'Claudia',
+            //         image:
+            //     },
+            //     {
+            //         name: 'Federico',
+            //         image:
+            //     },
+            //     {
+            //         name: 'Davide',
+            //         image:
+            //     },
+            // ]
+        }
+    }
+}).mount('#app')
