efa1e970558934cff8550d0615e50a47ffd5c5d0
martinaliporata <martinaliporata@gmail.com>
2024-06-02 17:37:07 +0200

    implementing v-for cycle

--- js/script.js
+++ js/script.js
@@ -3,40 +3,41 @@ const {createApp} = Vue
 createApp ({
     data(){
         return {
-            // contatti: [
-            //     {
-            //         name: 'Michele',
-            //         image: 
-            //     },
-            //     {
-            //         name: 'Fabio',
-            //         image:
-            //     },
-            //     {
-            //         name: 'Samuele',
-            //         image:
-            //     },
-            //     {
-            //         name: 'Alessandro B.',
-            //         image:
-            //     },
-            //     {
-            //         name: 'Alessandro L.',
-            //         imageUrl: './imgs/avatar_5.jpg',
-            //     },
-            //     {
-            //         name: 'Claudia',
-            //         image:
-            //     },
-            //     {
-            //         name: 'Federico',
-            //         image:
-            //     },
-            //     {
-            //         name: 'Davide',
-            //         image:
-            //     },
-            // ]
+            contatti: [
+                {
+                    name: 'Michele',
+                    image: './imgs/avatar_1.jpg'
+                },
+                {
+                    name: 'Fabio',
+                    image: './imgs/avatar_2.jpg'
+                },
+                {
+                    name: 'Samuele',
+                    image: './imgs/avatar_3.jpg'
+                },
+                {
+                    name: 'Alessandro B.',
+                    image: './imgs/avatar_4.jpg'
+                },
+                {
+                    name: 'Alessandro L.',
+                    imageUrl: './imgs/avatar_5.jpg',
+                },
+                {
+                    name: 'Claudia',
+                    image: './imgs/avatar_6.jpg'
+                },
+                {
+                    name: 'Federico',
+                    image: './imgs/avatar_7.jpg'
+                },
+                {
+                    name: 'Davide',
+                    image: './imgs/avatar_8.jpg'
+                },
+            ]
         }
-    }
+    },
+    methods
 }).mount('#app')
